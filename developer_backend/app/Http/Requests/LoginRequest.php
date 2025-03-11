28dc09fee36fa03d22ef62c1c988aa0e5b7fc8d8
MoeChanMyaeHtun <moechan599@gmail.com>
2025-03-11 14:43:44 +0630

    login json fixed

--- developer_backend/app/Http/Requests/LoginRequest.php
+++ developer_backend/app/Http/Requests/LoginRequest.php
@@ -24,8 +24,8 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email', 'max:255', Rule::exists(User::class, 'email')],
-            'password' => ['required', 'string', 'min:6'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string'],
         ];
     }
 }
