6c47d25c8a8ed4baf21effe0952ad02cb6163f92
MoeChanMyaeHtun <moechan599@gmail.com>
2025-03-21 15:37:55 +0630

    email verify bug fixed

--- developer_backend/app/Mail/EmailVerification.php
+++ developer_backend/app/Mail/EmailVerification.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
@@ -8,29 +9,44 @@ use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Support\Facades\URL;
 
-class EmailVerification extends Mailable {
-    
+class EmailVerification extends Mailable
+{
+
     use Queueable, SerializesModels;
 
     public $url;
+    // public function __construct($email)
+    // {
+    //     $generate = URL::temporarySignedRoute('verify-email',now()->addMinute(30),['email' => $email->email]);
+    //     $this->url = str_replace(config('app.url'), config('app.frontend_url'), $generate);
+    // }
     public function __construct($email)
     {
-        $generate = URL::temporarySignedRoute('verify-email',now()->addMinute(30),['email' => $email->email]);
-        
-        $this->url = str_replace(config('app.url'), config('app.frontend_url'), $generate);
-
+        $generate = URL::temporarySignedRoute(
+            'verify-email',
+            now()->addMinutes(30),
+            ['email' => $email->email]
+        );
+        $frontendUrl = rtrim(config('app.frontend_url'), '/');
+        $backendUrl = rtrim(config('app.url'), '/');
+        if (strpos($generate, $backendUrl) !== false) {
+            $this->url = str_replace($backendUrl, $frontendUrl, $generate);
+        } else {
+            $this->url = $frontendUrl . parse_url($generate, PHP_URL_PATH) . '?' . parse_url($generate, PHP_URL_QUERY);
+        }
     }
 
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject:'Email Verification'
+            subject: 'Email Verification'
         );
     }
 
-    public function content(): Content{
+    public function content(): Content
+    {
         return new Content(
-            markdown:'email_verification',
+            markdown: 'email_verification',
             with: ['verificationUrl' => $this->url]
         );
     }
