8b7a960f86879c2a07092dbabfaa8b1b1b7500fc
Phyo-Khant-Kyaw <www.pkkcobra01@gmail.com>
2025-04-22 23:22:37 +0700

    control dopay button event

--- src/pages/direct/include/DirectDoPay.jsx
+++ src/pages/direct/include/DirectDoPay.jsx
@@ -176,7 +176,7 @@ const DirectDoPay = ({
         <Button onClick={encryptPayData}>AES-128 ECB Encrypt</Button>
         <Button
           onClick={makeApiRequest}
-          disabled={!accessToken || !paymentToken || !encryptedString || apiResponse}
+          disabled={!accessToken || !paymentToken || !encryptedString || apiResponse?.respCode === "0000"}
         >
           Send Request
         </Button>
